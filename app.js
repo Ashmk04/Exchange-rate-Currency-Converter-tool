0a561e485e72ba737e67be9a881071ac17ccb07e
MUtkule Ashish <mutkuleashish1@gmail.com>
2026-05-09 14:15:49 +0530

    Restructured project files

---
+++ app.js
@@ -0,0 +1,51 @@
+const baseURL = "https://api.frankfurter.dev/v1/latest?amount=1&from=USD&to=INR"
+const dropdowns = document.querySelectorAll(".dropdown select");
+const button = document.querySelector("form button");
+// const fromCurr = document.querySelector(".from select").value;
+// const toCurr = document.querySelector(".to select").value;
+const result = document.querySelector(".result p");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "USD"){
+            newOption.selected = "selected";
+        }else if(select.name === "to" && currCode === "INR"){
+                newOption.selected = "selected";
+        }
+        select.append(newOption)
+    }
+    select.addEventListener( "change", (evt) =>{
+        updateFlag(evt.target);
+    });
+}
+
+
+const updateFlag = ((element) =>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc; 
+})
+
+button.addEventListener( "click" ,async(evt)=>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    // console.log(amtVal);
+    const fromCurr = document.querySelector(".from select").value;
+    const toCurr = document.querySelector(".to select").value;
+    // console.log(fromCurr);
+    // console.log(toCurr);
+    URL = `https://api.frankfurter.dev/v1/latest?amount=${amtVal}&from=${fromCurr}&to=${toCurr}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.rates[toCurr];
+    let finalAmt = amtVal * rate;
+    // console.log(finalAmt);
+    result.innerText = `${amtVal} ${fromCurr} = ${finalAmt} ${toCurr}`;
+});
+
